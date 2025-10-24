724c535283421b3f8d54b6ab104998a31ba92e10
Medhat Elmasry <medhat@elmasry.ca>
2025-10-24 15:06:56 -0700

    https://api.stgeorge.ca/ is base url for API

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,7 +1,7 @@
 export const API_BASE_URL = 
   window.location.hostname === 'localhost' 
     ? 'https://localhost:7031' 
-    : 'https://portal.stgeorge.ca/api';
+    : 'https://api.stgeorge.ca';
 
 export const API_ENDPOINTS = {
   EVENTS_CURRENT: `${API_BASE_URL}/events/current`,
