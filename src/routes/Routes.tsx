6d4a04aa9ad006195200b6aa61343561efc18005
Medhat Elmasry <medhat@elmasry.ca>
2025-10-24 14:50:52 -0700

    fixed unused component in routes.tsx

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom';
-import HomePage from "../pages/HomePage";
+// import HomePage from "../pages/HomePage";
 import App from "../App";
 import ArabicPage from "../pages/Arabic";
 import EnglishPage from "../pages/English";
